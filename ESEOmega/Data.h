0b854bfa1f1deca4296c57f16956904a76a68d35
tomn94 <tmnstudios@hotmail.fr>
2017-01-29 18:52:24 +0800

    – Unused deprecated URL_CONNECT login URL

--- ESEOmega/Data.h
+++ ESEOmega/Data.h
@@ -65,7 +65,6 @@
 #define URL_EVENT_SD @"https://web59.secure-secure.co.uk/francoisle.fr/api/event/send"
 #define URL_EVENT_ML @"https://web59.secure-secure.co.uk/francoisle.fr/api/event/mail"
 #define URL_INGENEWS @"https://web59.secure-secure.co.uk/francoisle.fr/api/ingenews?%d"
-#define URL_CONNECT  @"https://web59.secure-secure.co.uk/francoisle.fr/api/client/connect"
 #define URL_LOGIN    @"https://web59.secure-secure.co.uk/francoisle.fr/api/client/login"
 #define URL_PUSH     @"https://web59.secure-secure.co.uk/francoisle.fr/api/push/register"
 #define URL_UNPUSH   @"https://web59.secure-secure.co.uk/francoisle.fr/api/push/unregister"
