112cd15421cc0676eb96e39b7672df9e6bceeb2e
Thomas Naudet <tmnstudios@hotmail.fr>
2017-09-07 05:14:55 +0200

    ~ Split UserTVC file

---
+++ ESEOmega/Profil/UserTVC+Avatar.swift
@@ -0,0 +1,239 @@
+//
+//  UserTVC+Avatar.swift
+//  ESEOmega
+//
+//  Created by Tomn on 07/09/2017.
+//  Copyright © 2017 Thomas Naudet
+
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see http://www.gnu.org/licenses/
+//
+
+import UIKit
+
+
+// MARK: - Actions
+extension UserTVC {
+    
+    /// Finds the path to the user's avatar on disk
+    ///
+    /// - Returns: The URL of the image, if available
+    func getPhotoURL() -> URL? {
+        
+        /* Get documents directory */
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+                                                        .userDomainMask, true)
+        if paths.count > 0 {
+            let documentsDirectory = paths[0]
+            
+            /* The image file is stored right inside this documents directory */
+            return NSURL(fileURLWithPath: documentsDirectory).appendingPathComponent("imageProfil.png")
+        }
+        
+        return nil
+    }
+    
+    /// Load the user's avatar from disk
+    ///
+    /// - Returns: The data of the image, if available
+    func getPhoto() -> Foundation.Data? {
+        
+        /* Get the URL of the image file */
+        if let avatarURL = getPhotoURL() {
+            do {
+                /* Read data and return */
+                return try Foundation.Data(contentsOf: avatarURL)
+            } catch {}
+        }
+        
+        return nil
+    }
+    
+    /// Asks the user whether they want to change or delete their avatar
+    @objc func changePhoto() {
+        
+        /* Directly choose photo if there's already a picture */
+        if getPhoto() == nil {
+            selectPhoto()
+            return
+        }
+        
+        /* Ask what action to do */
+        let sheet = UIAlertController(title: "",
+                                      message: "Changer l'image de profil",
+                                      preferredStyle: .actionSheet)
+        
+        /* Configure actions */
+        sheet.addAction(UIAlertAction(title: "Supprimer la photo",
+                                      style: .destructive, handler: { _ in
+            self.removePhoto()
+        }))
+        sheet.addAction(UIAlertAction(title: "Choisir une photo",
+                                      style: .default, handler: { _ in
+            self.selectPhoto()
+        }))
+        sheet.addAction(UIAlertAction(title: "Annuler",
+                                      style: .cancel))
+        
+        self.present(sheet, animated: true)
+    }
+    
+    /// Displays a standard photo picker to select a new avatar
+    func selectPhoto() {
+        
+        /* The user will select a photo from the photos they have already taken */
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+        
+            /* Create an instance of an iOS image picker */
+            let imagePicker = UIImagePickerController()
+            imagePicker.sourceType    = .photoLibrary
+            imagePicker.delegate      = self
+            imagePicker.allowsEditing = true
+            
+            /* Show the picker in a pop-over on iPad, fullscreen if not */
+            if UIDevice.current.userInterfaceIdiom == .pad,
+               let avatarView = self.tableView.emptyDataSetView.imageView {
+                
+                /* Configure the picker as a pop-over */
+                imagePicker.modalPresentationStyle = .popover   // needs to be set 1st
+                
+                /* Place the pop-over on the screen */
+                let popPresentCtrl = imagePicker.popoverPresentationController
+                popPresentCtrl?.sourceRect = avatarView.convert(avatarView.bounds,
+                                                                to: UIApplication.shared.windows[0])
+                popPresentCtrl?.sourceView = imagePicker.view
+                popPresentCtrl?.permittedArrowDirections = .any
+                
+                /* Present pop-over */
+                self.present(imagePicker, animated: true)
+                
+            } else {
+                /* Present fullscreen on iPhone */
+                self.present(imagePicker, animated: true, completion: {
+                    /* Apply light status bar style to the image picker */
+                    UIApplication.shared.statusBarStyle = .lightContent
+                })
+            }
+        }
+    }
+    
+    /// Save a given picture to disk
+    ///
+    /// - Parameter picture: Picture to be saved as PNG
+    func savePhoto(_ picture: UIImage) {
+        
+        /* Get destination path and scale down picture */
+        if let saveURL = getPhotoURL(),
+           let scaledDownPic = Data.scaleAndCropImage(picture,
+                                                      to: CGSize(width: UserTVC.avatarImgSize, height: UserTVC.avatarImgSize),
+                                                      retina: false) {
+            
+            /* Create data representation */
+            let imgData = UIImagePNGRepresentation(scaledDownPic)
+            do {
+                /* Try to write data to destination */
+                try imgData?.write(to: saveURL)
+            } catch {}
+        }
+    }
+    
+    /// Deletes the current user picture from disk without confirmation
+    func removePhoto() {
+        
+        /* If the user has currently an avatar */
+        if getPhoto() != nil,
+           let avatarURL = getPhotoURL() {
+            do {
+                /* Delete it from disk */
+                try FileManager.default.removeItem(at: avatarURL)
+            } catch let error {
+                /* Display an error message if any issue arises */
+                let alert = UIAlertController(title: "Impossible de supprimer l'image",
+                                              message: error.localizedDescription,
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK",
+                                              style: .cancel))
+                self.present(alert, animated: true)
+            }
+        }
+        
+        /* Commit any change to the view */
+        animateChange()
+        self.refreshEmptyDataSet()
+    }
+    
+}
+
+
+// MARK: - Image Picker delegate
+
+extension UserTVC: UIImagePickerControllerDelegate {
+    
+    /// Called when the user chose a picture from their library thanks to the image picker
+    ///
+    /// - Parameters:
+    ///   - picker: The controller of the image picker
+    ///   - info: Used to get the selected picture back
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        /* Get the chosen image */
+        guard let chosenImage = info[UIImagePickerControllerEditedImage] as? UIImage
+            else { return }
+        
+        /* Save it to disk */
+        savePhoto(chosenImage)
+        
+        /* Update the avatar */
+        animateChange()
+        self.refreshEmptyDataSet()
+        
+        /* Dismiss the picker */
+        self.dismiss(animated: true) {
+            UIApplication.shared.statusBarStyle = .lightContent     // Apply app style back
+        }
+    }
+    
+    /// Called when the user cancelled the operation of changing avatar
+    ///
+    /// - Parameter picker: The controller of the image picker
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        /* Dismiss the picker */
+        self.dismiss(animated: true) {
+            UIApplication.shared.statusBarStyle = .lightContent     // Apply app style back
+        }
+    }
+    
+}
+
+
+// MARK: - Navigation Controller delegate
+
+extension UserTVC: UINavigationControllerDelegate {
+    
+    /// Called when the user chooses an album from their library
+    ///
+    /// - Parameters:
+    ///   - navigationController: The picker navigation controller
+    ///   - viewController: The new controller being presented
+    ///   - animated: Whether the presentation is animated
+    func navigationController(_ navigationController: UINavigationController,
+                              willShow viewController: UIViewController,
+                              animated: Bool) {
+        
+        /* Apply app style to the picker */
+        UIApplication.shared.statusBarStyle = .lightContent
+    }
+    
+}
---
+++ ESEOmega/Profil/UserTVC+Login.swift
@@ -0,0 +1,291 @@
+//
+//  UserTVC+Login.swift
+//  ESEOmega
+//
+//  Created by Tomn on 07/09/2017.
+//  Copyright © 2017 Thomas Naudet
+
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see http://www.gnu.org/licenses/
+//
+
+import UIKit
+
+
+extension UserTVC {
+    
+    // MARK: - Login
+    
+    /// Sends data to connection API and reacts accordingly
+    func connect() {
+        
+        /* Hide keyboard */
+        mailField.resignFirstResponder()
+        passField.resignFirstResponder()
+        
+        guard checkConnect() else { return }
+        
+        /* Disable send button */
+        configureSendCell(mail: "", password: "")
+        
+        /* CONNECT TO API */
+        
+        /* Create URL encoded POST attributes */
+        let cleanMail = mailField.text?.trimmingCharacters(in: .whitespacesAndNewlines).lowercased() ?? ""
+        let password  = self.passField.text ?? ""
+        
+        /* Set URL Session */
+        let urlSession = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
+        let urlRequest = API.request(.userLogin, get: ["email"    : cleanMail,
+                                                       "password" : password])
+        
+        /* Set data callback */
+        let dataTask = urlSession.dataTask(with: urlRequest) { (data, urlResponse, error) in
+            
+            /* Stop loading indicators */
+            Utils.requiresActivityIndicator(false)
+            self.spin.stopAnimating()
+            
+            /* Allow Send button to be tapped again */
+            self.configureSendCell(mail: self.mailField.text, password: password)
+            
+            guard let d = data, error == nil,
+                  let jsonData = try? JSONSerialization.jsonObject(with: d),
+                  let json     = jsonData as? [String : Any],
+                  let success  = json["success"] as? Bool else {
+                /* Present unknown error due to parsing */
+                self.connectionFailed()
+                return
+            }
+            
+            /* If connection error */
+            guard success else {
+                if let error =  json["error"]       as? [String : Any],
+                   let cause = error["userMessage"] as? String,
+                   let uid   = error["uid"]         as? Int {
+                    self.connectionFailed(error: cause, code: uid)
+                } else {
+                    self.connectionFailed()
+                }
+                return
+            }
+            
+            /* If connected */
+            guard let username = json["fullname"] as? String,
+                  let tokenJWT = json["token"]    as? String else {
+                self.connectionFailed(error: "Appelez Champollion, impossible de déchiffrer vos informations.")
+                return
+            }
+            
+            /* Validated, save data */
+            DataStore.connectUser(name: username, mail: cleanMail, token: tokenJWT)
+            
+            /* Get user's orders
+               Since it's a tab, it's very probable they're currently on it, or right after */
+            Data.shared().updateJSON("cmds")
+            
+            /* Alert other views */
+            NotificationCenter.default.post(name: .connectionStateChanged, object: nil)
+            
+            /* Present greeting message */
+            self.connectionSucceeded(for: username)
+        }
+        
+        /* Fire connection */
+        Utils.requiresActivityIndicator(true)
+        spin.startAnimating()
+        dataTask.resume()
+    }
+    
+    /// Checks connection parameters (mail, password) and blocks if too many attempts
+    ///
+    /// - Returns: True if no error, the connection can be established
+    func checkConnect() -> Bool {
+        
+        /* Get mail (clean an lowercased) and password values */
+        guard let mail = mailField.text?.trimmingCharacters(in: .whitespacesAndNewlines).lowercased(),
+              let pass = passField.text,
+              mail != "",
+              pass != "" else { return false }
+        
+        /* Give a try at the current date */
+        UserTVC.attemptsNbr += 1
+        let currentTimeInterval = Date.timeIntervalSinceReferenceDate
+        
+        /* In case the limit has been hit */
+        let onTooManyAttempts = {
+            
+            /* Display an integer of the remaning minutes to wait */
+            let minToWait = Int(ceil((UserTVC.maxAttemptsWaitingTime - currentTimeInterval + UserTVC.lastMaxAttempt) / 60))
+            let unit = "minute" + (minToWait > 1 ? "s" : "")
+            
+            /* Present error message */
+            let alert = UIAlertController(title: "Doucement",
+                                          message: "Veuillez attendre \(minToWait) \(unit), vous avez réalisé trop de tentatives à la suite.",
+                                          preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Mince !", style: .cancel))
+            
+            self.present(alert, animated: true)
+        }
+        
+        /* If the user has hit the maximum */
+        if UserTVC.attemptsNbr == UserTVC.maxAttempts + 1 {
+            
+            /* Start the countdown */
+            UserTVC.lastMaxAttempt = currentTimeInterval
+            
+            /* Display error message and cancel */
+            onTooManyAttempts()
+            return false
+        }
+        else if UserTVC.attemptsNbr > UserTVC.maxAttempts + 1 {
+
+            /* If enough time has passed, set a normal number of attempts */
+            if currentTimeInterval - UserTVC.lastMaxAttempt > UserTVC.maxAttemptsWaitingTime {
+                UserTVC.attemptsNbr = 1
+            } else {
+                /* If the user still has to wait, display an error message and cancel */
+                onTooManyAttempts()
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    /// Displays an alert confirming login was a success, and inits push
+    ///
+    /// - Parameters:
+    ///   - username: Customize welcome message with the name of the user
+    func connectionSucceeded(for username: String) {
+        
+        /* Set default values */
+        var title = "Bienvenue"
+        
+        /* Customize with name if available */
+        let formatter = PersonNameComponentsFormatter()
+        if #available(iOS 10.0, *),
+           let nameComponents = formatter.personNameComponents(from: username),
+           let firstName = nameComponents.givenName {
+            title += ", \(firstName)"
+        }
+        else if let firstName = username.components(separatedBy: " ").first {
+            title += ", \(firstName)"
+        }
+        title += " !"
+        
+        /* Present alert box */
+        let alert = UIAlertController(title: title,
+                                      message: "Vous êtes connecté, vous bénéficiez désormais de la commande à la cafétéria/événements.\n\nPour être notifié lorsque votre repas est prêt, veuillez accepter les notifications !",
+                                      preferredStyle: .alert)
+        
+        /* Custom message whether the user has already push notifications */
+        let hasPushEnabled = Data.shared().pushToken != nil
+        alert.addAction(UIAlertAction(title: hasPushEnabled ? "Parfait" : "Parfait, j'y penserai !",
+                                      style: .cancel,
+                                      handler: { _ in
+            /* Register for push notifications */
+            if let delegate = UIApplication.shared.delegate as? AppDelegate {
+                Data.registeriOSPush(delegate)
+            }
+            
+            /* Sync the device push token with the server to allow future push */
+            if hasPushEnabled &&
+               JNKeychain.loadValue(forKey: KeychainKey.token) != nil {
+                Data.sendPushToken()
+            }
+            
+            /* Close the whole profile panel */
+            self.close()
+        }))
+        
+        self.present(alert, animated: true)
+    }
+    
+    /// Displays an alert explaining why login has failed
+    ///
+    /// - Parameters:
+    ///   - error: Description of the cause of the error, default alert texts if empty
+    ///   - code: Error code
+    func connectionFailed(error: String = "", code: Int = 0) {
+        
+        /* Set default error messages */
+        var title   = "Impossible de valider votre connexion sur nos serveurs"
+        var message = "Impossible de valider votre connexion sur nos serveurs. Si le problème persiste, contactez-nous."
+        
+        /* Use a description of the error instead if provided */
+        if error != "" {
+            title   = code == 7 ? "Oups…" : "Erreur"  // customize if wrong password
+            message = error
+        }
+        
+        /* Show alert with message */
+        let alert = UIAlertController(title: title, message: message,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .cancel))
+        self.present(alert, animated: true)
+    }
+    
+    
+    // MARK: - Logout
+    
+    /// Asks the user whether they are sure to logout, and eventually do it
+    @objc func disconnect() {
+        
+        /* Display an alert to confirm the choice */
+        let alert = UIAlertController(title: "Voulez-vous vraiment vous déconnecter ?",
+                                      message: "Vos éventuelles commandes en cours à la cafétéria restent dues.",
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Annuler",
+                                      style: .cancel))
+        
+        alert.addAction(UIAlertAction(title: "Se déconnecter",
+                                      style: .destructive,
+                                      handler: { _ in
+            
+            /* Delete any avatar from disk */
+            if self.getPhoto() != nil {
+                self.removePhoto()
+            }
+            
+            /* Delete all profile data */
+            DataStore.disconnectUser()
+                                        
+            /* Remove user's orders
+               Since it's a tab, it's very probable they're currently on it, or right after */
+            Data.shared().updateJSON("cmds")
+            
+            /* Display connection form and appropriate navigation bar buttons */
+            self.animateChange()
+
+            /* Reset default theme */
+            ThemeManager.currentTheme = .common
+            // Repaint the navigation bar
+            if let userNavController = self.navigationController {
+                ThemeManager.updateTheme(of: userNavController)
+            }
+                                        
+            self.loadUI()
+            self.tableView.reloadData()
+            
+            /* Alert other views */
+            NotificationCenter.default.post(name: .connectionStateChanged, object: nil)
+        }))
+        
+        self.present(alert, animated: true)
+    }
+    
+}
