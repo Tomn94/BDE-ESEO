b0cd81cdabc7c8e819c3448ca3c4d50c76bef31f
Thomas Naudet <tmnstudios@hotmail.fr>
2017-11-11 20:31:24 +0100

    + Interface configuration

--- BDE-Watch Extension/CafetInterfaceController.swift
+++ BDE-Watch Extension/CafetInterfaceController.swift
@@ -23,7 +23,7 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class CafetInterfaceController: WKInterfaceController {
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
--- BDE-Watch Extension/RoomsInterfaceController.swift
+++ BDE-Watch Extension/RoomsInterfaceController.swift
@@ -23,7 +23,7 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class RoomsInterfaceController: WKInterfaceController {
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
